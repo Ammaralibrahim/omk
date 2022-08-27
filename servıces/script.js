a775fb5c2fabd051a585733edd30015e069faec3
pro-programmer-ammar <96020759+Ammaralibrahim@users.noreply.github.com>
2022-08-28 00:50:55 +0300

    Refresh services page

--- servıces/script.js
+++ servıces/script.js
@@ -1,15 +1,56 @@
-const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
-
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
-
-options.forEach(option =>{
-    option.addEventListener("click", ()=>{
-        let selectedOption = option.querySelector(".option-text").innerText;
-        sBtn_text.innerText = selectedOption;
-
-        optionMenu.classList.remove("active");
-    });
-});
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("b");
+let webLink;
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
---
+++ servıces/suggestions.js
@@ -0,0 +1,23 @@
+let suggestions = [
+    "Acıbadem Maslak Hastanesi",
+    "Amerikan Hastanesi",
+    "Medipol Üniversitesi Hastanesi",
+    "İstanbul Üniversitesi Hastaneleri",
+    "Acıbadem Altunizade Hastanesi",
+    "İstanbul Florence Nightingale Hastanesi",
+    "Koç Üniversitesi Hastanesi",
+    "Marmara Üniversitesi Hastanesi",
+    "Memorial Bahçelievler Hastanesi",
+    "Academic Hospital",
+    " Memorial Şişli Hastanesi",
+    "İstanbul Florence Nightingale",
+    "NP İstanbul Beyin Hastanesi",
+    "Medical Park Grubu",
+    "Medicana Çamlıca Hastanesi",
+    "Kolan Uluslararası Hastanesi",
+    "Liv Hospital Ulus",
+    "Emsey Hastanesi",
+    "Avrasya Hastanesi",
+    
+   
+];
